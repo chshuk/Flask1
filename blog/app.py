4c25e7bae768249e59685a684ada13bd594a4156
Alexandra Timoshchuk <chshuk@mail.ru>
2023-03-13 22:25:06 +0300

    Lesson_03 SQL-Alchemy

--- blog/app.py
+++ blog/app.py
@@ -4,12 +4,16 @@ from flask import Flask, request, g, render_template
 from blog.views.users import users_app
 from blog.views.reports import reports
 from blog.views.articles import articles_app
+from blog.models.database import db
 
 
 app = Flask(__name__)
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(reports, url_prefix="/reports")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 
 @app.route("/")
