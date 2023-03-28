4466295f43a58b09e880baff9dcee7fd1e3eadf8
Александра Тимощук <97471611+chshuk@users.noreply.github.com>
2023-03-28 14:10:38 +0300

    lesson_07 tags (#7)

--- blog/commands.py
+++ blog/commands.py
@@ -11,6 +11,22 @@ def create_init_user():
 
     with app.app_context():
         db.session.add(
-            User(email='name@example.com', password=generate_password_hash('test123'))
+            User(first_name='name',
+                 last_name='surname',
+                 email='name@example.com',
+                 password=generate_password_hash('test123'))
         )
         db.session.commit()
+
+
+@click.command('create-init-tags')
+def create_init_tags():
+    from blog.models import Tag
+    from wsgi import app
+
+    with app.app_context():
+        tags = ('flask', 'django', 'python', 'gb', 'sqlite')
+        for item in tags:
+            db.session.add(Tag(name=item))
+        db.session.commit()
+    click.echo(f'Created tags: {", ".join(tags)}')
