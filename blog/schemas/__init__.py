3e871591c0bf9cc56267ec28375ee25c67107df7
Alexandra Timoshchuk <chshuk@mail.ru>
2023-03-29 13:20:42 +0300

    lesson_09 API

---
+++ blog/schemas/__init__.py
@@ -0,0 +1,12 @@
+from blog.schemas.tag import TagSchema
+from blog.schemas.user import UserSchema
+from blog.schemas.article import ArticleSchema
+from blog.schemas.author import AuthorSchema
+
+
+__all__ = [
+    'TagSchema',
+    'UserSchema',
+    'AuthorSchema',
+    'ArticleSchema',
+]
