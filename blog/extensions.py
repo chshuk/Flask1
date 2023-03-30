e884d371ab3112939fa67cb8e236f9a152df9eef
Александра Тимощук <97471611+chshuk@users.noreply.github.com>
2023-03-30 11:51:08 +0300

    lesson_09 API (#9)

--- blog/extensions.py
+++ blog/extensions.py
@@ -1,8 +1,10 @@
 from flask_admin import Admin
+from flask_combo_jsonapi import Api
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+
 from blog.admin.views import CustomAdminIndexView
 
 login_manager = LoginManager()
@@ -14,3 +16,4 @@ admin = Admin(
     name='Blog Admin Panel',
     template_mode='bootstrap4',
 )
+api = Api()
